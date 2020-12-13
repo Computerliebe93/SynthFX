c9c64cb2daeecb143d6c278685d2daa10f8a22c6
bakerbear47 <47745594+bakerbear47@users.noreply.github.com>
2020-12-13 22:30:31 +0100

    wrap main in try catch

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,21 +6,29 @@ public class Main extends Application  {
 
     @Override
     public void start(Stage primaryStage) {
-        Synth synth = new Synth();
-        MidiKeyboard midiKeyboard = new MidiKeyboard(synth);
-        Controller controller = new Controller(synth);
-        View view = new View(synth, controller);
-        synth.setController(controller);
-        synth.setView(view);
-        synth.setMidiKeyboard(midiKeyboard);
-        primaryStage.setTitle("Grandaddy");
-        primaryStage.setScene(new Scene(view.asParent(), 800, 600));
-        controller.setView(view);
-        primaryStage.show();
+        try{
+            Synth synth = new Synth();
+            MidiKeyboard midiKeyboard = new MidiKeyboard(synth);
+            Controller controller = new Controller(synth);
+            View view = new View(synth, controller);
+            synth.setController(controller);
+            synth.setView(view);
+            synth.setMidiKeyboard(midiKeyboard);
+            primaryStage.setTitle("Grandaddy");
+            primaryStage.setScene(new Scene(view.asParent(), 800, 600));
+            controller.setView(view);
+            primaryStage.show();
 
-        //new  thread for model
-        Thread thread = new Thread(synth);
-        thread.start();
+            //new  thread for model
+            Thread thread = new Thread(synth);
+            thread.start();
+
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
     public static void main (String[]args) {launch (args);}{
     }
